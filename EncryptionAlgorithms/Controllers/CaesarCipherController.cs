5041190780aa7de0b2c1c7cb9484577574d88053
amirhanna2001 <amirhanna111200@gmail.com>
2023-04-27 21:56:39 +0200

    CesarCipher Algorithm

---
+++ EncryptionAlgorithms/Controllers/CaesarCipherController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptionAlgorithms.Controllers
+{
+    public class CaesarCipherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public static string Encrypt(string plaintext, int shift)
+        {
+            string ciphertext = "";
+
+            foreach (char c in plaintext)
+            {
+                if (Char.IsLetter(c))
+                {
+                    // Determine the position of the character in the alphabet (0-25)
+                    int position = Char.ToUpper(c) - 'A';
+
+                    // Apply the shift and wrap around if necessary
+                    int shiftedPosition = (position + shift) % 26;
+
+                    // Convert the shifted position back to a character
+                    char shiftedChar = (char)('A' + shiftedPosition);
+
+                    // Add the shifted character to the ciphertext
+                    ciphertext += shiftedChar;
+                }
+                else
+                {
+                    // Leave non-alphabetic characters unchanged
+                    ciphertext += c;
+                }
+            }
+
+            return ciphertext;
+        }
+
+        // Decrypts ciphertext using a Caesar cipher with the given shift value
+        public static string Decrypt(string ciphertext, int shift)
+        {
+            // To decrypt, simply shift in the opposite direction
+            return Encrypt(ciphertext, 26 - shift);
+        }
+    }
+}
